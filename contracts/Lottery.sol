87430172f4c3c9de416e20442d6f8a47ca7bc897
Santiago Herrera Blanco <santiago@glue.digital>
2022-01-28 12:22:41 +0000

    Contract optimized

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -9,7 +9,6 @@ contract Lottery is ReentrancyGuard, Ownable {
     uint256 public lotteryPrize = 10000000000000000000;
     IERC20 public token;
 
-    mapping(uint16 => bool) public purchased;
     mapping(uint16 => address) public tickets;
 
     constructor(address _owner, address _token) {
@@ -23,13 +22,12 @@ contract Lottery is ReentrancyGuard, Ownable {
      */
     function buyLottery(uint16 _ticket) public nonReentrant {
         require(_ticket > 0 && _ticket <= 10000, "Ticket number should be more than 1 and less than 10000.");
-        require(!purchased[_ticket], "Ticket not available.");
+        require(tickets[_ticket] == address(0), "Ticket not available.");
         uint256 _userBalance = token.balanceOf(msg.sender);
         require(_userBalance >= lotteryPrize, "Insufficient Balance.");
 
         token.transferFrom(msg.sender, address(this), lotteryPrize);
 
-        purchased[_ticket] = true;
         tickets[_ticket] = msg.sender;
     }
 }
